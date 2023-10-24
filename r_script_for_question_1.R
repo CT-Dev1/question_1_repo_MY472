83e1a0f9b8a182e863e1d424fe03486a8173edda
CT-Dev1 <ryanhrousseau@gmail.com>
2023-10-24 18:49:12 +0100

    first rough draft of function with 2 inputs

--- r_script_for_question_1.R
+++ r_script_for_question_1.R
@@ -4,12 +4,16 @@ library(tidyverse)
 #loading the data
 data <- InsectSprays
 
-#defining function that outputs a table with the minimum insect prevalence recorded by each pesticide 
-find_minimum_insects <- function(data){
+#defining function that outputs a table with the minimum insect prevalence recorded by each pesticide
+#two inputs - data and number of insect count to display in ascending order
+find_minimum_insects <- function(data,numbers_to_display){
   data %>% 
     group_by(spray) %>% 
-    summarize(Min_Insects = min(count))
+    arrange(count) %>% 
+    slice(1:numbers_to_display) %>% 
+    summarize(spray = spray, minimum = slice(1:numbers_to_display))
+    
 }
 
 #testing the function on InsectSprays data
-find_minimum_insects(data)
+find_minimum_insects(data,3)
